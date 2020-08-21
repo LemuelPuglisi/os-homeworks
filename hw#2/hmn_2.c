8c7f05f2775b9893e48fdb3fd1ab9dcc2afbdc95
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2020-08-21 16:27:57 +0200

    feat: complete homework 2

--- hw#2/hmn_2.c
+++ hw#2/hmn_2.c
@@ -1,10 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+
+#include <string.h>
+#include <strings.h>
+#include <libgen.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <libgen.h>
+#include <sys/sysmacros.h>
+
+/**
+ * throw errno error specifying the error source 
+ * 
+ * @param char * source
+ */  
+void throw_perror(char *source)
+{
+    char * str = malloc(strlen(source) + 15); 
+    sprintf(str, "[error | %s]", source); 
+    perror(str);
+	exit(-1);
+}
+
+/**
+ * terminate the program whit a message 
+ * 
+ * @param char * message
+ */  
+void throw_error_with_message(char *message)
+{
+    fprintf(stderr, "[error] %s \n", message); 
+	exit(-1);
+}
+
+/**
+ * take the input resource path and extract the name. Then 
+ * append this name in the destination_base string. For 
+ * example: 
+ * 
+ * resource_path:       /var/log/test.log
+ * destination_base:    /home/log/
+ * output:              /home/log/test.log
+ * 
+ * @param char * message
+ * @return char * resource rebased 
+ */ 
+char * move_resource_base (char * resource_path, char * destination_base)
+{
+    char * filename = basename(resource_path); 
+    char * resource_rebased = malloc(strlen(filename) + strlen(destination_base)); 
+    strcat(resource_rebased, destination_base); 
+
+    if (destination_base[strlen(destination_base) - 1] != '/') 
+        strcat(resource_rebased, "/");    
+
+    strcat(resource_rebased, filename);
+    return resource_rebased;  
+}
 
 /*
     Homework n.2
@@ -23,36 +78,55 @@
 
 int main (int argc, char* argv[])  {
 
-    struct stat *srcinfo, *tgtinfo; 
-    char *srcpath, *tgtpath; 
-    int cmdresponse = 0; 
+    struct stat srcinfo, dstinfo; 
+    char *srcpath, *dstpath; 
+    int same_file_system = 1, 
+        is_symbolic_link = 0; 
 
-    if (argc != 3) {
-        printf("usage: %s <source pathname> <destination pathname>\n", argv[0]); 
-        exit(1); 
-    } 
-    else {
+    if (argc == 3) {
         srcpath = argv[1]; 
-        tgtpath = dirname(argv[2]); 
+        dstpath = argv[2]; 
+    }
+    else throw_error_with_message("usage: move <source pathname> <destination pathname>"); 
+
+    if (lstat(srcpath, &srcinfo) == -1 || stat(dstpath, &dstinfo) == -1) {
+        throw_perror("stat"); 
     }
 
-    cmdresponse |= lstat(srcpath, srcinfo) * -1;
-    cmdresponse |=  stat(tgtpath, tgtinfo) * -1;
+    // get the new file path 
+    char * dst_file_path = move_resource_base(srcpath, dstpath); 
 
-    if (cmdresponse == 1) {
-        printf("[error] invalid path\n"); 
-        exit(1); 
+    if (S_ISLNK(srcinfo.st_mode)) {
+        
+        // moving a symlink 
+        const short link_content_buffer_size = 2048; 
+        char link_content_buffer[link_content_buffer_size]; 
+        readlink(srcpath, link_content_buffer, link_content_buffer_size);
+        symlink(link_content_buffer, dst_file_path);  
+        unlink(srcpath);
+
     }
+    else if (srcinfo.st_dev == dstinfo.st_dev) {
 
-    // controllo il file system di entrambi i path
-    if (srcinfo->st_dev == tgtinfo->st_dev) {
+        // same file system, using rename function. 
+        rename(srcpath, dst_file_path); 
 
-        // hard link copy and remove original one
-        printf("same file system \n"); 
     }
     else {
 
-        // different file system 
-        printf("diff file system \n"); 
+        // regular file, different file system. Must copy and delete. 
+        const short buffer_dim = 1024;
+        char buffer[buffer_dim];
+        
+        FILE * src_file = fopen(srcpath, "r");
+        FILE * dst_file = fopen(dst_file_path, "w+"); 
+         
+        while (fgets(buffer, buffer_dim - 1, src_file))
+            fprintf(dst_file, "%s", buffer); 
+        
+        fclose(src_file); 
+        fclose(dst_file);
+        unlink(srcpath); 
     }
+
 }
